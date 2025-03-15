f927391f5889f5b4b21e711cd6406297cafdebee
Ziqiang Li <fypetro@gmail.com>
2025-03-15 00:17:16 -0600

    Implement cmake add_subdirectory recursively

--- app/s2edit/main.cpp
+++ app/s2edit/main.cpp
@@ -8,9 +8,9 @@
 int main(int argc, char const *argv[])
 {
 	std::cout << "s2edit version "
-		<< _S2EDIT_VERSION_MAJOR << "."
-		<< _S2EDIT_VERSION_MINOR << "."
-		<< _S2EDIT_VERSION_PATCH << "\n";
+		<< _S2LIKELIB_VERSION_MAJOR << "."
+		<< _S2LIKELIB_VERSION_MINOR << "."
+		<< _S2LIKELIB_VERSION_PATCH << "\n";
 
 	Point p(0., 0.);
 	std::cout << p << "\n";
